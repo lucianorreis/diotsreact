691d2e58345e7be5378dccb05fe4bb9a14797384
Luciano.Reis <lrr.arms233@gmail.com>
2024-11-16 13:27:03 -0300

    Aprendendo a usar propriedades em React

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,20 @@
-import React from 'react';
-import { Card } from './components/Card'
-import { Footer } from './components/Footer'
-import { Header } from './components/Header'
+import React from "react";
+import { Card } from "./components/Card";
+import { Layout } from "./components/Layout";
 
 function App() {
   return (
-  <>
-    <Header />
-    <div>
-      <Card 
-        id={1}
-        paragraph='TypeScript'
-        details='TS para Frontend e Backend'
+    <>
+      <Layout>
+        <Card
+          id={1}
+          paragraph="TypeScript"
+          details="TS para Frontend e Backend"
         />
-      <Card 
-        id={2}
-        paragraph='HTML'
-        details='HTML para Frontend e Backend'
-        />
-      <Card 
-        id={3}
-        paragraph='SQL'
-        details='SQL para banco de dados'
-        />
-    </div>
-    <Footer />
-  </>
+        <Card id={2} paragraph="HTML" details="HTML para Frontend e Backend" />
+        <Card id={3} paragraph="SQL" details="SQL para banco de dados" />
+      </Layout>
+    </>
   );
 }
 
